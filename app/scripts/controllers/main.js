3d8edbf3f54d66bbfc9f4615e01c6990192fa8de
csctwx <csctwx@msn.com>
2016-04-26 13:59:41 -0400

    init

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name canadaInsuranceApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the canadaInsuranceApp
+ */
+angular.module('canadaInsuranceApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
